570c50ef37cbe9d08ed9a07f9492718f5969d8b1
haodo22 <haodo2285@gmail.com>
2025-09-25 15:30:34 +0700

    Base Dự Án

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Button } from 'antd';
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-red-500 text-5xl font-bold underline">
+        Hello world!
+      </h1>
+      <div className="p-4">
+        <Button type="primary">Antd Button</Button>
+      </div>
+    </div>
+
+  )
+}
+
+export default App
